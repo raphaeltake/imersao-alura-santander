0f39bc16be526fd7bce312bb84f85a6b04bd303f
raphaeltake <raph.takeuchi@gmail.com>
2025-11-25 22:49:29 -0300

    if-else e switch case

--- dart/calculadora/bin/calculadora.dart
+++ dart/calculadora/bin/calculadora.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 
 void main() {
-  double num1 = double.parse(stdin.readLineSync()!);
-  double num2 = double.parse(stdin.readLineSync()!);
+  double num1 = 0;
+  // num1 = double.parse(stdin.readLineSync()!);
+  double num2 = 0;
+  // num2 = double.parse(stdin.readLineSync()!);
+
+  String operacao = "";
+  // operacao = stdin.readLineSync()!;
   // print(num1 + num2);
 
   void soma(){
@@ -21,10 +26,55 @@ void main() {
     print(num1 * num2);
   }
 
-  soma();
-  subtracao();
-  divisao();
-  multiplicacao();
+  void calcular(){
+    switch (operacao){
+        case "+": soma();
+        case "-": subtracao();
+        case "*": multiplicacao();
+        case "/": divisao();
+        break;
+    }
+  }
+
+  print("Digite o primeiro valor: ");
+  String? entrada = stdin.readLineSync();
+  if (entrada != null){
+    if (entrada != ""){
+      num1 = double.parse(entrada);
+    }
+  }
+
+  print("Digite uma operação: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    operacao = entrada;
+  }
+  
+  print("Digite o segundo valor: ");
+  entrada = stdin.readLineSync();
+  if (entrada != null){
+    if (entrada != ""){
+      num2 = double.parse(entrada);
+    }
+  }
+
+  
+
+  // if (operacao == "+"){
+  //   soma();
+  // } else if (operacao == "-"){
+  //   subtracao();
+  // } else if (operacao == "/"){
+  //   divisao();
+  // } else if (operacao == "*"){
+  //   multiplicacao();
+  // } else{
+  //   print("Essa operação não existe!");
+  // }
+
+  print("O resultado da operação é: ");
+
+  calcular();
 
 }
 
