a4e62f1f8ac6b309514862b4e397429b07a15540
raphaeltake <raph.takeuchi@gmail.com>
2025-11-30 01:27:00 -0300

    Capturando pacotes

--- dart/dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
+++ dart/dominando assincronismo e criando comunicação com APIs/dart_assincronismo/bin/main.dart
@@ -1,7 +1,8 @@
 import 'package:http/http.dart';
-
+import 'dart:convert';
 void main() {
-  requestData();
+  // requestData();
+  requestDataAsync();
 }
 
 requestData(){
@@ -10,5 +11,16 @@ requestData(){
   // print(futureResponse);
   futureResponse.then((Response response) {
     print(response.body);
+    List<dynamic> listAccounts = json.decode(response.body);
+    Map<String, dynamic> mapCarla = listAccounts.firstWhere((element) => element["name"] == "Carla");
+    print(mapCarla["balance"]);
   },);
+  
+}
+
+requestDataAsync() async {
+  String url = "https://gist.githubusercontent.com/raphaeltake/8e8668fbf14472b191a6488f0700772f/raw/3dbc73134cc1f6f37c2298493420cbdeff39e015/accounts.json";
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
+
 }
