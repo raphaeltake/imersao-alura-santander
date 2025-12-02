7de6b53387a7251ce7d9578305727c6637f63b73
raphaeltake <raph.takeuchi@gmail.com>
2025-12-01 22:00:20 -0300

    Lidando com Streams e modelos update

--- dart/dominando assincronismo e criando comunicação com APIs/dart_assincronismo/lib/models.account.dart
+++ dart/dominando assincronismo e criando comunicação com APIs/dart_assincronismo/lib/models.account.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Account {
   String id;
   String name;
@@ -11,16 +13,57 @@ class Account {
     required this.balance,
   });
 
-  factory Account.fromMap(Map<String, dynamic> map){
-    return Account(id: map["id"], name: map["name"], lastName: map["lastName"], balance: map["balance"]);
+  factory Account.fromMap(Map<String, dynamic> map) {
+    return Account(
+      id: map["id"],
+      name: map["name"],
+      lastName: map["lastName"],
+      balance: map["balance"],
+    );
   }
 
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return <String, dynamic>{
       "id": id,
       "name": name,
       "lastName": lastName,
       "balance": balance,
     };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  Account copyWith({
+    String? id,
+    String? name,
+    String? lastName,
+    double? balance,
+  }) {
+    return Account(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      lastName: lastName ?? this.lastName,
+      balance: balance ?? this.balance,
+    );
+  }
+
+  @override
+  String toString() {
+    return '\\nConta $id\\n$name $lastName\\nSaldo: $balance\\n';
+  }
+
+  @override
+  bool operator ==(covariant Account other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id &&
+        other.name == name &&
+        other.lastName == lastName &&
+        other.balance == balance;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^ name.hashCode ^ lastName.hashCode ^ balance.hashCode;
   }
 }
